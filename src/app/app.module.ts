1060999b0f72b393d62bad83a99ab68db3c07a8b
sirarthurnell <sirarthurnell@gmail.com>
2018-08-25 15:58:22 +0200

    Some refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MonthCalendarComponent } from './components/month-calendar/month-calendar.component';
 import { DayTemplateDirective } from './directives/day-template.directive';
-import { DayOfWeekTemplateDirective } from './directives/day-of-week-template.directive';
-import { MonthTemplateDirective } from './directives/month-template.directive';
+import { DayOfWeekCaptionTemplateDirective } from './directives/day-of-week-caption-template.directive';
+import { MonthCaptionTemplateDirective } from './directives/month-caption-template.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     MonthCalendarComponent,
     DayTemplateDirective,
-    DayOfWeekTemplateDirective,
-    MonthTemplateDirective
+    DayOfWeekCaptionTemplateDirective,
+    MonthCaptionTemplateDirective
   ],
   imports: [
     BrowserModule,
